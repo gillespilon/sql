d56a44d282e16e0c132a119b2266935c47a4b07f
Gilles Pilon <gillespilon13@gmail.com>
2020-10-15 18:43:56 -0500

    Add read sql

--- food.py
+++ food.py
@@ -11,27 +11,6 @@ import datasense as ds
 import pandas as pd
 import sqlite3
 
-# connection = sqlite3.connect('food.db')
-# cursor = connection.cursor()
-
-# list tables in database
-# cursor.execute(
-#     "SELECT name FROM sqlite_master\
-#     WHERE type = 'table'\
-#     ORDER BY name;"
-# )
-# print(cursor.fetchall())
-# print()
-
-# read table into dataframe
-# query = "SELECT * FROM groceries;"
-# df = pd.read_sql(query, connection)
-# print(df)
-# print()
-
-# cursor.execute("DROP table 'test'")
-# save dataframe to new database
-# df.to_sql('test', connection, if_exists='replace', index = False)
 
 
 def main():
@@ -55,6 +34,25 @@ def main():
     )
     print(cursor.fetchall())
     print()
+    # open a database and read a table into a dataframe
+    # list tables in database
+    cursor.execute(
+        "SELECT name FROM sqlite_master\
+        WHERE type = 'table'\
+        ORDER BY name;"
+    )
+    print(cursor.fetchall())
+    print()
+
+    # read table into dataframe
+    query = "SELECT * FROM my_groceries;"
+    df = pd.read_sql(query, connection)
+    print(df.head())
+    print(df.dtypes)
+
+    # cursor.execute("DROP table 'test'")
+    # save dataframe to new database
+    # df.to_sql('test', connection, if_exists='replace', index = False)
 
 
 def create_dataframe() -> pd.DataFrame:
